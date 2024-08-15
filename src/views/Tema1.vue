1e177de1a3a7e55e6273b72ccf133cfc2128b1b3
Luis Perez <ljpm9401@gmail.com>
2024-08-14 20:37:23 -0500

    Avances CF

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,7 +5,156 @@
     .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Contrato de trabajo
+    
+    .row
+      .col-lg-8.mb-4.mb-lg-0(data-aos="fade-right")
+        p La legislación laboral de Colombia, reglamentada en el Código Sustantivo del Trabajo (CST), es la norma que regula todo lo que tiene que ver con la situación laboral del talento humano en las organizaciones.
+        p La definición de lo qué es el contrato en Colombia se encuentra en el Capítulo I. Definición y normas generales. Art. 22 del C.S.T., el cual se cita textualmente para conocer lo que expresa la ley sobre el particular:
+        .d-flex.mt-4.col-xl-10.cont_1_1
+          i.fas.fa-quote-left(style="color:#3CEAD6;font-size:38px;")
+          .ps-3.mt-2
+            p #[strong 1.] Es aquel por el cual una persona natural se obliga a prestar un servicio personal a otra persona natural o jurídica, bajo la continuada dependencia o subordinación de la segunda y mediante remuneración. 
+            p.mb-0 #[strong 2.] Quien presta el servicio se denomina trabajador, quien lo recibe y remunera, empleador, y la remuneración, cualquiera que sea su forma, salario.
+            .text-end(style="margin-top:-16px;")
+              i.fas.fa-quote-right(style="color:#3CEAD6;font-size:38px;")
+      .col-lg-4(data-aos="fade-left")
+        figure.m-auto.col-8.col-md-7.col-lg-12
+          img(src="@/assets/curso/tema1/1.png")
+    
+    .row.mt-5
+      .col-lg-8.mb-4.mb-lg-0(data-aos="fade-right")
+        p Los elementos del contrato se encuentran señalados en el C.S.T. Art. 23, el cual es muy específico y relaciona los elementos sobre el particular, estos elementos que en la ley describen y tipifican cuando se da una relación contractual, son: 
+        .d-flex.mt-4.col-xl-10.cont_1_1
+          i.fas.fa-quote-left(style="color:#3CEAD6;font-size:38px;")
+          .ps-3.mt-3
+            .d-flex
+              h5 La actividad personal del trabajador, es decir, realizada por sí mismo. La continuada subordinación o dependencia del trabajador respecto del patrono, que faculta a este para exigirle el cumplimiento de órdenes… Un salario como retribución del servicio.
+              i.fas.fa-quote-right(style="color:#3CEAD6;font-size:38px;place-self:end;")
+      .col-lg-4(data-aos="fade-left")
+        figure.m-auto.col-8.col-md-7.col-lg-12
+          img(src="@/assets/curso/tema1/2.svg")
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_1_1 1.1  Contenido del contrato
+    
+    p(data-aos="fade-right") Cuando se pacta entre las partes intervinientes, es decir, patrono y empleado, un contrato, en este caso laboral, debe perfeccionarse de acuerdo con la legislación laboral en Colombia, la cual reglamenta las cláusulas y los aspectos generales a desarrollar según las características, para que tenga vida jurídica. 
+    p(data-aos="fade-left") Los elementos que contiene el contrato de trabajo son: 
+
+    .cont_1_2.mt-4.pb-4.pt-4.pb-xl-5.pt-xl-5
+      .row
+        .col-lg-4.mb-4.mb-lg-0(data-aos="fade-right")
+          figure.m-auto.col-8.col-md-7.col-lg-12
+            img(src="@/assets/curso/tema1/4.png")
+        .col-lg-8(data-aos="fade-left" style="place-self:center;")
+          SlyderF(columnas="col-lg-6 p-4")
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/5.svg")
+              p.mt-4.fw-bold Identificación y domicilio de las partes.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/6.svg")
+              p.mt-4.fw-bold Labor o trabajo a ejecutar.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/7.svg")
+              p.mt-4.fw-bold El lugar del trabajo.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/8.svg")
+              p.mt-4.fw-bold Salario.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/9.svg")
+              p.mt-4.fw-bold Forma y período de pago.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/10.svg")
+              p.mt-4.fw-bold Lugar y fecha de celebración.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/11.svg")
+              p.mt-4.fw-bold El período de prueba.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/12.svg")
+              p.mt-4.fw-bold Las obligaciones.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/13.svg")
+              p.mt-4.fw-bold Las prohibiciones.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/14.svg")
+              p.mt-4.fw-bold Causales especiales de terminación.
+            .tarjeta.text-center.p-4.cont_1_3
+              figure
+                img(src="@/assets/curso/tema1/15.svg")
+              p.mt-4.fw-bold Las correspondientes firmas de las partes intervinientes.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_1_2 1.2 Clases de contratos
+    
+    p(data-aos="fade-right") Cuando se pacta entre las partes intervinientes, es decir, patrono y empleado, un contrato, en este caso laboral, debe perfeccionarse de acuerdo con la legislación laboral en Colombia, la cual reglamenta las cláusulas y los aspectos generales a desarrollar según las características, para que tenga vida jurídica. 
+    p(data-aos="fade-left") Los elementos que contiene el contrato de trabajo son: 
+
+    .row.mt-4
+      .col-lg-3.d-none.d-lg-block(data-aos="fade-right")
+        figure
+          img(src="@/assets/curso/tema1/16.png")
+      .col-lg-9(data-aos="fade-left")
+        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row.col-xl-11.m-auto(titulo="Por la forma en que se suscriben")
+            p Definidos en el Capítulo IV del C.S.T. Modalidades del contrato (forma, contenido, duración, revisión, suspensión y prueba del contrato) Art. 37: “El contrato de trabajo puede ser verbal o escrito; para su validez no requiere forma especial alguna, salvo disposición expresa en contrario”. 
+          .row.col-xl-11.m-auto(titulo="Por la duración del contrato")
+            p.fw-bold Duración indefinida
+            p Art. 47 C.S.T. Es importante estar revisando la normativa que trata estos temas, porque constantemente es modificada por parte de la Rama Legislativa. Actualmente, la ley hace referencia sobre este tipo de contrato, señalando que tendrá vigencia mientras subsistan las causas que le dieron origen y la materia del trabajo (num. 2. C.S.T.). 
+            p.fw-bold.mt-4 Contrato término fijo
+            p Artículo subrogado por el artículo 3o. de la Ley 50 de 1990. El nuevo texto es el siguiente: el contrato de trabajo a término fijo debe constar siempre por escrito y su duración no puede ser superior a tres años, pero es renovable indefinidamente.
+    
+    .row.mt-4
+      .col-lg-9(data-aos="fade-right")
+        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row.col-xl-11.m-auto(titulo="Contrato duración de la obra o labor determinada")
+            p Art. 45 C.S.T: “en este tipo de contratos, muy utilizados en actividades o el ramo de la construcción, al tratarse de vigencias muy concretas se pacta una obra con plazos de entrega, últimamente se utilizan para transferencia de conocimiento se vincula profesionales o expertos en áreas concretas para que desarrollen investigaciones”.
+          .row.col-xl-11.m-auto(titulo="Contrato de aprendizaje")
+            p Art. 30 Ley 789 de 2002. Se suele utilizar este tipo de contrato en aprendices SENA (formaciones para el desarrollo humano), con la promesa de que al terminar su etapa de formación ingrese a la empresa a desarrollar su práctica laboral. La ley ha establecido el proceso de contratación, teniendo en cuenta el tamaño de las organizaciones, así mismo, será el número de aprendices a contratar. 
+      .col-lg-3.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema1/17.png")
+    
+    .cont_1_4.mt-5.pb-5
+      p.fw-bold(style="color:#8746F8;" data-aos="fade-up") Importante
+      p(data-aos="fade-right") Para el contrato a término, fijo (Decreto 2663 de 150):
+      .row.mt-4
+        .col-lg-8.mb-4.mb-lg-0(data-aos="fade-right")
+          ol.lista-ol--cuadro.mb-0
+            li.d-flex
+              .lista-ol--cuadro__vineta(style="background-color:#3CEAD6;")
+                span.fw-bold(style="color:initial;") 1.
+              span Si antes de la fecha del vencimiento del término estipulado, ninguna de las partes avisara por escrito a la otra su determinación de no prorrogar el contrato, con una antelación no inferior a treinta (30) días, este se entenderá renovado por un período igual al inicialmente pactado, y así sucesivamente.
+            li.d-flex.mb-0
+              .lista-ol--cuadro__vineta(style="background-color:#3CEAD6;")
+                span.fw-bold(style="color:initial;") 2.
+              span No obstante, si el término fijo es inferior a un (1) año, únicamente podrá prorrogarse sucesivamente el contrato hasta por tres (3) períodos iguales o inferiores, al cabo de los cuales el término de renovación no podrá ser inferior a un (1) año, y así sucesivamente.
+          .p-3.mt-3(style="background-color:#D7FAF6;border-radius:14px;")
+            p.mb-0 PARÁGRAFO. En los contratos a término fijo inferior a un año, los trabajadores tendrán derecho al pago de vacaciones y prima de servicios en proporción al tiempo laborado cualquiera que este sea.
+        .col-lg-4(data-aos="fade-left")
+          figure.m-auto.col-8.col-md-7.col-lg-12
+            img(src="@/assets/curso/tema1/18.svg")
+
+
+
+
+            
+
+
+
       
 </template>
 
@@ -26,5 +175,4 @@ export default {
   },
 }
 </script>
-
 <style lang="sass"></style>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,95 @@
     .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Inducción, capacitación, adiestramiento y evaluación del desempeño
+    
+    p(data-aos="fade-right") Luego de la contratación, la empresa realiza una inducción, capacitación, adiestramiento y evaluación del desempeño, los cuales se explican a continuación.
+
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_2_1 2.1 Inducción
+    
+    .cont_2_1
+      .row
+        .col-lg-7(data-aos="fade-right")
+          p En el momento que se realiza la vinculación o se finiquita por ambas partes (empleador, empleado), el contrato de la persona que hará parte de la organización, es responsabilidad del área de talento humano. De igual manera, proporcionarles a los colaboradores o cliente interno la información básica sobre los antecedentes y la filosofía misional de la organización, para desempeñarse en sus actividades de manera satisfactoria. 
+          .p-3.mt-4(style="background-color:#CFCFE4;border-radius:14px;")
+            .mb-2(style="width:100px;height:8px;background-color:#8746F8;")
+            p.mb-0 Los objetivos propuestos en la inducción, son dar a conocer a todo el talento humano de la organización, la filosofía, políticas, normas, reglamento interno y seguridad laboral, generalmente es el líder del área quien realiza la inducción al nuevo personal, utilizando los elementos que su experiencia le dicta. 
+          p.mt-4 En organizaciones con manejo administrativo y gerencial, se delega a la gestión del talento humano esta responsabilidad, la cual con expertos, diseñan en sus planes de acción, programas o tipos de inducción. Cada organización elige lo que deben conocer los colaboradores en el momento de ingreso a sus puestos de trabajo. Esta información debe ser importante para el correcto desempeño en las funciones que realizará. 
+          p Básicamente existe dos tipos de inducción: 
+        .col-lg-5.d-none.d-lg-block(data-aos="fade-left")
+          figure
+            img(src="@/assets/curso/tema2/1.png")
+      
+      .col-xl-8.m-auto.mt-5
+        .row.justify-content-center
+          .col-lg.mb-4.mb-lg-0.col-md-8(data-aos="fade-right")
+            .h-100.text-center(style="background-color:#E6E6F1;border-radius:10px;")
+              figure
+                img(src="@/assets/curso/tema2/2.png")
+              .p-2.ps-4.pe-4.text-white.m-auto.fw-bold(style="background-color:#8746F8;border-radius:30px;max-width:fit-content;font-size:14px;top:-18px;position:relative;") General
+              .p-4.pt-2
+                p Es la información que se le entrega al colaborador o cliente interno de la organización, para que se apropie de la razón de ser (incluye la misión y la visión).
+          .col-lg.mb-4.mb-lg-0.col-md-8(data-aos="fade-left")
+            .h-100.text-center(style="background-color:#E6E6F1;border-radius:10px;")
+              figure
+                img(src="@/assets/curso/tema2/3.png")
+              .p-2.ps-4.pe-4.text-white.m-auto.fw-bold(style="background-color:#8746F8;border-radius:30px;max-width:fit-content;font-size:14px;top:-18px;position:relative;") Específica
+              .p-4.pt-2
+                p Es la información pormenorizada que debe conocer el colaborador o cliente interno, sobre sus funciones específicas y el área de trabajo para desempeñarse mejor (puesto de trabajo).
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_2_2 2.2 Capacitación
+    
+    .row
+      .col-lg-7.mb-4.mb-lg-0(data-aos="fade-right")
+        p Hoy en día existen diversas organizaciones (outsourcing) o, como se ha venido relacionando, personas naturales (tercerización), dedicadas a prestar este tipo de transferencia de conocimiento como un valor agregado, para que la empresa dedique toda su capacidad productiva (rentabilidad, crecimiento empresarial) al logro de sus objetivos.
+        p Se ha podido demostrar que los colaboradores que son capacitados en sus puestos de trabajo, tienen más sentido de pertenencia y son más comprometidos al logro de las metas organizacionales, porque también redunda en beneficios personales (ascensos laborales, cambio en su entorno social y laboral).
+      .col-lg-5(data-aos="fade-left")
+        figure.m-auto.col-9.col-md-7.col-lg-12
+          img(src="@/assets/curso/tema2/4.png")
+    
+    .cont_2_2
+      Separador
+
+      .titulo-segundo(data-aos="flip-up")
+        h2#t_2_3 2.3 Adiestramiento
+      
+      .row
+        .col-lg-9.mb-4.mb-md-0.col-md-6(data-aos="fade-right" style="place-self:center;")
+          p Es el proceso mediante el cual se desarrollan técnicas de enseñanza-aprendizaje, buscando mejorar y especializar los saberes de los colaboradores y desarrollar habilidades y destrezas, para que logren los objetivos en su puesto de trabajo.
+        .col-lg-3.col-md-6(data-aos="fade-left" style="place-self:center;")
+          figure.m-auto.col-9.col-lg-12
+            img(src="@/assets/curso/tema2/6.svg")
+
+      Separador
+
+      .titulo-segundo(data-aos="flip-up")
+        h2#t_2_4 2.4  Evaluación del desempeño
+
+      .row
+        .col-lg-7.mb-4.mb-lg-0(data-aos="fade-right")
+          p Con esta técnica se pretende retroalimentar al talento humano en procura de mejorar los procesos que realiza de manera sistemática. Incluye la revisión del comportamiento laboral para reconocer sus méritos y corregir sus debilidades, y lo que se pretende cuando se realiza, es tomar decisiones de selección, ascensos y salarios. Permite llevar un registro histórico de clima laboral y generalmente se realiza cada año o cuando se concluye el contrato.
+          p Dentro de las ventajas de la evaluación del talento humano están:
+        .col-lg-5(data-aos="fade-left")
+          figure.m-auto.col-9.col-md-7.col-lg-12
+            img(src="@/assets/curso/tema2/7.png")
+      
+      .col-xl-8.m-auto.col-lg-10.mt-5
+        .titulo-sexto.color-secundario(data-aos="zoom-in-down")
+          h5 Figura 1.
+          span Proceso de evaluación
+        figure(data-aos="zoom-in-up")
+          img.d-none.d-md-block(src="@/assets/curso/tema2/8.svg" alt="Imagen que presenta el proceso de evaluación en una empresa.")
+          img.d-block.d-md-none(src="@/assets/curso/tema2/9.svg" alt="Imagen que presenta el proceso de evaluación en una empresa.")
+
+
+
+
 
 </template>
 
@@ -25,5 +113,4 @@ export default {
   },
 }
 </script>
-
 <style lang="sass"></style>
